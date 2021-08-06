33b8cce99f2f8be49ab697a4ff5da095fd0a460c
Taylor Bird <nsubordin81@gmail.com>
2021-08-05 20:49:14 -0400

    type feature
    in an attempt to boost learning, I updated the type of random method used in the OUNoise in accordance with what I'd heard others have tried, though mentors said it shouldn't make a difference in the noise implementation. I also changed the network architecture and that seems to have resulted in more initial reward, but learning doesn't continue to improve.

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -158,6 +158,7 @@ class OUNoise:
         self.mu = mu * np.ones(size)
         self.theta = theta
         self.sigma = sigma
+        self.size = size
         self.seed = random.seed(seed)
         self.decay = 1
         self.reset()
@@ -169,8 +170,8 @@ class OUNoise:
     def sample(self):
         """Update internal state and return it as a noise sample."""
         x = self.state
-        dx = self.theta * (self.mu - x) + self.sigma * np.array(
-            [random.random() for i in range(len(x))]
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.standard_normal(
+            self.size
         )
         self.state = x + dx
         return self.state * self.decay
